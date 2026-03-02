6ce4a64cab187486d22930ab5b12a9e467bab079
henriquevenusto33-pato <henriquevenusto33@gmail.com>
2026-03-02 19:25:59 -0300

    alguma mudança

--- nossa.py
+++ nossa.py
@@ -1,1 +1,21 @@
-print(oi)
+class Jogo():
+    def __init__(self, nome, tipo, desenvolvedora):
+        self.nome = nome
+        self.tipo = tipo
+        self.desenvolvedora = desenvolvedora
+    def comprar(self):
+        return f"{self.nome} da {self.desenvolvedora} foi comprado"
+    
+    def baixar(self):
+        return f"{self.nome} esta classificado como {self.tipo}"
+    
+jogo1 = Jogo("hollow kight", "Metroidvania", "Team Cherry")
+jogo2 = Jogo("Elden Ring", "Soulsborne", "Fromsoftware")
+jogo3 = Jogo("minecraft", "rpg,mundo aberto", "mojang")
+
+print(jogo1.comprar())
+print(jogo1.baixar())
+print(jogo2.comprar())
+print(jogo2.baixar())
+print(jogo3.comprar())
+print(jogo3.baixar())
